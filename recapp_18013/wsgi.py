bcf1eb3b41d7895155fcdddba3e74514567e5e0a
Crowdbotics <team@crowdbotics.com>
2020-06-11 07:05:18 +0000

    Update models

--- recapp_18013/wsgi.py
+++ recapp_18013/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recapp_18013.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recapp_18013.settings")
 
 application = get_wsgi_application()
